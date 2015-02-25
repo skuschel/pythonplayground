7db0a23931672b62a759d47e7a3a3c7900623cfb
Stephan Kuschel <stephan.kuschel@gmail.com>
2015-02-25 20:56:00 +0100

    add sum of numpy array

--- example.py
+++ example.py
@@ -10,3 +10,15 @@ print 'sum'
 print examplemodule.sum(3,8)
 
 
+import numpy as np
+data = np.random.random(2000)
+print data.sum()
+print examplemodule.sumarray(data)
+
+import timeit
+n = 10000
+t = timeit.Timer(lambda: data.sum())
+print 'numpy: {:0.4e}'.format(t.timeit(number=n)/n)
+t = timeit.Timer(lambda: examplemodule.sumarray(data))
+print 'c: {:0.4e}'.format(t.timeit(number=n)/n)
+
--- examplemodule.c
+++ examplemodule.c
@@ -1,4 +1,7 @@
 #include <Python.h>
+#include "numpy/arrayobject.h"
+#include <stdio.h>
+#include <math.h>
 
 static PyObject* multiply(PyObject* self, PyObject* args)
 {
@@ -14,13 +17,37 @@ static PyObject* sum(PyObject* self, PyObject* args)
     return Py_BuildValue("d", (x+y)/2);
 }
 
+static PyObject* sumarray(PyObject* self, PyObject* args)
+{
+    PyArrayObject *array;
+    double ret, *carray;
+    int i, n;
+
+    if (!PyArg_ParseTuple(args, "O!",
+        &PyArray_Type, &array))  return NULL;
+    if (NULL == array)  return NULL;
+    //if (not_doublevector(array)) return NULL;
+
+    n = array->dimensions[0];
+    carray = (double *) array->data;
+
+    ret=0.0;
+    for (i=0; i<n; i++) {
+        ret += carray[i];
+        //printf("%.4f\n", ret);
+    }
+    return Py_BuildValue("d", ret);
+}
+
 static PyMethodDef examplemodule_methods[] = {
     {"multiply", multiply, METH_VARARGS},
     {"sum", sum, METH_VARARGS},
+    {"sumarray", sumarray, METH_VARARGS},
     {NULL, NULL}
 };
 
 void initexamplemodule()
 {
     (void) Py_InitModule("examplemodule", examplemodule_methods);
+    import_array();  //necessary for numpy
 }
