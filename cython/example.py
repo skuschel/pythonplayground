d1176b6bc17fdfdccc1ee70533e2057c3d04f384
Stephan Kuschel <stephan.kuschel@gmail.com>
2015-03-10 11:13:57 +0100

    cython sumarray slow

--- cython/example.py
+++ cython/example.py
@@ -7,5 +7,19 @@ import examplemodule as em
 print em.sum(7.1, 3.33)
 print em.sum(4,1)
 
+import numpy as np
 
+data = np.random.rand(2e6)
+datav = data[::2]
+print 'numpy summe:    {:.17e}'.format(datav.sum())
+print 'cython summe:   {:.17e}'.format(em.sumarray(datav))
 
+import timeit
+n = 100
+t = timeit.Timer(lambda: datav.sum())
+tn = t.timeit(number=n)/n
+print 'numpy:  {:0.4e} s'.format(tn)
+t = timeit.Timer(lambda: em.sumarray(datav))
+tc = t.timeit(number=n)/n
+print 'cython: {:0.4e} s'.format(tc)
+print 'Faktor {:5.4f} schneller!'.format(tn/tc)
--- cython/examplemodule.pyx
+++ cython/examplemodule.pyx
@@ -3,3 +3,20 @@ cimport cython
 
 cpdef double sum(double x, double y):
     return x + y
+
+
+# http://docs.cython.org/src/tutorial/numpy.html
+
+import numpy as np
+cimport numpy as np
+
+ctypedef np.double_t DTYPE_t
+
+def sumarray(np.ndarray data):
+    cdef DTYPE_t ret = 0.0
+    cdef int n = len(data)
+
+    for i in xrange(n):
+        ret += data[i]
+
+    return ret
