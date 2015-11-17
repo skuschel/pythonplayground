0723664e77f81cb090729f550c36063ea674b0b6
Stephan Kuschel <stephan.kuschel@gmail.com>
2015-11-17 23:42:46 +0100

    tornado: add simple websocket demo

--- tornado/serverdemo.py
+++ tornado/serverdemo.py
@@ -9,6 +9,7 @@ localhost:8000
 
 import tornado.web
 import tornado.ioloop
+from tornado import websocket
 import numpy as np
 import time
 import subprocess
@@ -20,10 +21,46 @@ class MainPageHandler(tornado.web.RequestHandler):
         self.write('<a href="/prim/65535">Primfaktorenzerlegung</a><br>')
         self.write('<a href="/uptime">show uptime</a><br>')
 
+GLOBALS = {'hellosockets' :[]}
+
+class HelloClientSocket(websocket.WebSocketHandler):
+    def open(self):
+        GLOBALS['hellosockets'].append(self)
+        print("WebSocket opened. Total sockets open: {:}".format(len(GLOBALS['hellosockets'])))
+
+    def on_close(self):
+        GLOBALS['hellosockets'].remove(self)
+        print("WebSocket closed. Total sockets open: {:}".format(len(GLOBALS['hellosockets'])))
+
+class HelloPushHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        data = self.get_argument('data')
+        for socket in GLOBALS['hellosockets']:
+            socket.write_message(data)
+        self.write('Posted: {:}'.format(data))
 
 class HalloWeltHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('Hallo Welt')
+        page = '''
+<html>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script language="javascript" type="text/javascript">
+$(document).ready(function () {
+    var ws = new WebSocket("ws://localhost:8000/hellosocket");
+    ws.onmessage = function(event) {
+       $('body').append('<div>' + event.data + '</div>');
+    }
+    $('body').append('<div> goto localhost:8000/hellopost?data=msg to post msg on this page.</div>');
+});
+</script>
+</head>
+<body>
+Hallo Welt!
+</body>
+</html>
+'''
+        self.write(page)
 
 
 class PrimfaktorHandler(tornado.web.RequestHandler):
@@ -57,6 +94,8 @@ class UptimeHandler(tornado.web.RequestHandler):
 handlers = [
             ('/', MainPageHandler),
             ('/hello', HalloWeltHandler),
+            ('/hellosocket', HelloClientSocket),
+            ('/hellopush', HelloPushHandler),
             ('/prim/([0-9]+)', PrimfaktorHandler),
             ('/uptime', UptimeHandler),
            ]
